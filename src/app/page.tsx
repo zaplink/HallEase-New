204104fb8073bf7740df2f44bdf20d21d5cbdc54
pathummadhusanka <pathum23200@gmail.com>
2025-01-23 15:45:21 +0530

    Add shadcn button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
-    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <div style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
       <h1 style={{ fontSize: '4rem', fontWeight: 'bold' }}>Home</h1>
+      <Button>Click me</Button>
     </div>
   );
 }
