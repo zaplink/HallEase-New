1e70fd80aae88e613be0e45a3a669a19407b1147
pathummadhusanka <pathum23200@gmail.com>
2025-01-23 15:38:51 +0530

    Add pages: Login, Event, Hall

--- src/app/hall/page.tsx
+++ src/app/hall/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <h1 style={{ fontSize: '4rem', fontWeight: 'bold' }}>Home</h1>
+      <h1 style={{ fontSize: '4rem', fontWeight: 'bold' }}>Hall</h1>
     </div>
   );
 }
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Login() {
   return (
     <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
-      <h1 style={{ fontSize: '4rem', fontWeight: 'bold' }}>Home</h1>
+      <h1 style={{ fontSize: '4rem', fontWeight: 'bold' }}>Login</h1>
     </div>
   );
 }
