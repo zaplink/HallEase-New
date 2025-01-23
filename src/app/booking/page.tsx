b4a43ba380ccec0155d15d41367e2adeb0ea8260
pathummadhusanka <pathum23200@gmail.com>
2025-01-23 20:35:52 +0530

    Add page: Booking

---
+++ src/app/booking/page.tsx
@@ -0,0 +1,7 @@
+export default function Boking() {
+  return (
+    <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+      <h1 style={{ fontSize: '4rem', fontWeight: 'bold' }}>Booking</h1>
+    </div>
+  );
+}
