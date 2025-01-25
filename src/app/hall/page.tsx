558a5168c9e9abda96794fd459efd530d8c8c22f
FathimaNafra <fathumanafra5@gmail.com>
2025-01-25 13:39:15 +0530

    add some design to the table

--- src/app/hall/page.tsx
+++ src/app/hall/page.tsx
@@ -11,48 +11,49 @@ import { FaInfoCircle } from "react-icons/fa";
 
 export default function Home() {
   return (
-    <div style={{  justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+    <div style={{  justifyContent: 'center', alignItems: 'center', height: '100vh',padding:'20px' }}>
+
       
-      <Table >
+      <Table style={{width:'60%',borderCollapse:'collapse',marginLeft:'20%',marginTop:'10%'}}>
   
-  <TableHeader >
+  <TableHeader style={{backgroundColor:'#f4f4f4'}}>
     <TableRow >
-      <TableHead >Hall</TableHead>
-      <TableHead >Capacity</TableHead>
-      <TableHead >Status</TableHead>
-      <TableHead >Actions</TableHead>
+      <TableHead style={{padding:'12px',textAlign:'center',fontWeight:'bold',color:'#333',fontSize:'15px'}}>Hall</TableHead>
+      <TableHead style={{padding:'12px',textAlign:'center',fontWeight:'bold',color:'#333',fontSize:'15px'}}>Capacity</TableHead>
+      <TableHead style={{padding:'12px',textAlign:'center',fontWeight:'bold',color:'#333',fontSize:'15px'}}>Status</TableHead>
+      <TableHead style={{padding:'12px',textAlign:'center',fontWeight:'bold',color:'#333',fontSize:'15px'}}>Actions</TableHead>
     </TableRow>
   </TableHeader>
   <TableBody>
-    <TableRow >
-      <TableCell >AB-LCH-1</TableCell>
-      <TableCell >50</TableCell>
-      <TableCell >Available</TableCell>
-      <TableCell >
+    <TableRow style={{ borderBottom: '1px solid #ddd' }}>
+      <TableCell style={{padding:'12px',color:'#555',textAlign:'center'}}>AB-LCH-1</TableCell>
+      <TableCell style={{padding:'12px',color:'#555',textAlign:'center'}}>50</TableCell>
+      <TableCell style={{padding:'12px',color:'#555',textAlign:'center'}}>Available</TableCell>
+      <TableCell style={{padding:'12px',color:'#555', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
         <FaInfoCircle size={24} />
       </TableCell>
     </TableRow>
-    <TableRow >
-      <TableCell >AB-LCH-1</TableCell>
-      <TableCell >50</TableCell>
-      <TableCell >Available</TableCell>
-      <TableCell >
+    <TableRow style={{ borderBottom: '1px solid #ddd' }}>
+      <TableCell style={{padding:'12px',color:'#555',textAlign:'center'}}>AB-LCH-1</TableCell>
+      <TableCell style={{padding:'12px',color:'#555',textAlign:'center'}}>50</TableCell>
+      <TableCell style={{padding:'12px',color:'#555',textAlign:'center'}}>Available</TableCell>
+      <TableCell style={{padding:'12px',color:'#555', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
         <FaInfoCircle size={24} />
       </TableCell>
     </TableRow>
-    <TableRow >
-      <TableCell >AB-LCH-1</TableCell>
-      <TableCell >50</TableCell>
-      <TableCell >Available</TableCell>
-      <TableCell >
+    <TableRow style={{ borderBottom: '1px solid #ddd' }}>
+      <TableCell style={{padding:'12px',color:'#555',textAlign:'center'}}>AB-LCH-1</TableCell>
+      <TableCell style={{padding:'12px',color:'#555',textAlign:'center'}}>50</TableCell>
+      <TableCell style={{padding:'12px',color:'#555',textAlign:'center'}}>Available</TableCell>
+      <TableCell style={{padding:'12px',color:'#555', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
         <FaInfoCircle size={24} />
       </TableCell>
     </TableRow>
-    <TableRow >
-      <TableCell >AB-LCH-1</TableCell>
-      <TableCell >50</TableCell>
-      <TableCell >Available</TableCell>
-      <TableCell >
+    <TableRow style={{ borderBottom: '1px solid #ddd' }}>
+      <TableCell style={{padding:'12px',color:'#555',textAlign:'center'}}>AB-LCH-1</TableCell>
+      <TableCell style={{padding:'12px',color:'#555',textAlign:'center'}}>50</TableCell>
+      <TableCell style={{padding:'12px',color:'#555',textAlign:'center'}}>Available</TableCell>
+      <TableCell style={{padding:'12px',color:'#555', display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
         <FaInfoCircle size={24} />
       </TableCell>
     </TableRow>
