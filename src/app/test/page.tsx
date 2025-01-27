dcff92da0913c0886a1c99b684f0252fdc98b0f1
pathummadhusanka <pathum23200@gmail.com>
2025-01-27 18:57:10 +0530

    chore: add code-formatting using pre-commit hook
    - Setup Husky, lint-staged and Prettier to format code before commit

--- src/app/test/page.tsx
+++ src/app/test/page.tsx
@@ -1,53 +1,53 @@
-import SidebarLayout from "@/layouts/SidebarLayout";
-import { Input } from "@/components/ui/input";
+import SidebarLayout from '@/layouts/SidebarLayout';
+import { Input } from '@/components/ui/input';
 import {
-    Table,
-    TableHeader,
-    TableHead,
-    TableBody,
-    TableRow,
-    TableCell,
-} from "@/components/ui/table";
-import { Info } from "lucide-react";
+	Table,
+	TableHeader,
+	TableHead,
+	TableBody,
+	TableRow,
+	TableCell,
+} from '@/components/ui/table';
+import { Info } from 'lucide-react';
 
 export default function Test() {
-    return (
-        // Sidebar layout
-        <SidebarLayout>
-            <div className="mb-6 flex flex-row">
-                {/* Search halls */}
-                <Input placeholder="Search Halls" />
-            </div>
+	return (
+		// Sidebar layout
+		<SidebarLayout>
+			<div className='mb-6 flex flex-row'>
+				{/* Search halls */}
+				<Input placeholder='Search Halls' />
+			</div>
 
-            {/* Table */}
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead>Hall ID</TableHead>
-                        <TableHead>Capacity</TableHead>
-                        <TableHead>Status</TableHead>
-                        <TableHead>Actions</TableHead>
-                    </TableRow>
-                </TableHeader>
-                <TableBody>
-                    <TableRow>
-                        <TableCell>AB-LCH-01</TableCell>
-                        <TableCell>100</TableCell>
-                        <TableCell>Available</TableCell>
-                        <TableCell>
-                            <Info size={20} />
-                        </TableCell>
-                    </TableRow>
-                    <TableRow>
-                        <TableCell>AB-LCH-02</TableCell>
-                        <TableCell>50</TableCell>
-                        <TableCell>Booked</TableCell>
-                        <TableCell>
-                            <Info size={20} />
-                        </TableCell>
-                    </TableRow>
-                </TableBody>
-            </Table>
-        </SidebarLayout>
-    );
+			{/* Table */}
+			<Table>
+				<TableHeader>
+					<TableRow>
+						<TableHead>Hall ID</TableHead>
+						<TableHead>Capacity</TableHead>
+						<TableHead>Status</TableHead>
+						<TableHead>Actions</TableHead>
+					</TableRow>
+				</TableHeader>
+				<TableBody>
+					<TableRow>
+						<TableCell>AB-LCH-01</TableCell>
+						<TableCell>100</TableCell>
+						<TableCell>Available</TableCell>
+						<TableCell>
+							<Info size={20} />
+						</TableCell>
+					</TableRow>
+					<TableRow>
+						<TableCell>AB-LCH-02</TableCell>
+						<TableCell>50</TableCell>
+						<TableCell>Booked</TableCell>
+						<TableCell>
+							<Info size={20} />
+						</TableCell>
+					</TableRow>
+				</TableBody>
+			</Table>
+		</SidebarLayout>
+	);
 }
