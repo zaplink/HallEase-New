cb6d5ce630158ab6d5846b9e989b168811cff4e2
pathummadhusanka <pathum23200@gmail.com>
2025-01-28 12:44:32 +0530

    chore: add empty dashboard page

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,7 @@
+export default function Dashboard() {
+	return (
+		<div className='flex items-center justify-center h-screen'>
+			<h1 className='text-4xl font-bold'>Dashboard</h1>
+		</div>
+	);
+}
