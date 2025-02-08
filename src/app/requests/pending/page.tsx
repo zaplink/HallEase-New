4130a2bb783c5d455f73e97b24535d59dd59a95e
Sitharahansamali <hsithara69@gmail.com>
2025-02-08 16:09:25 +0530

    add pending page

---
+++ src/app/requests/pending/page.tsx
@@ -0,0 +1,13 @@
+import Requestpage from "@/components/ui/Requestpage";
+import SidebarLayout from "@/layouts/SidebarLayout";
+
+export default function pending() {
+    return (
+      <>
+      <SidebarLayout>
+           <Requestpage />
+      </SidebarLayout>
+       
+      </>
+    );
+  }
