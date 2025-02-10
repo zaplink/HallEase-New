5863f16829630c1f36c58dc34cd4be2f45dc7e4e
pathummadhusanka <pathum23200@gmail.com>
2025-02-10 23:19:34 +0530

    chore: change dashboard layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -3,7 +3,7 @@ import SidebarLayout from '@/layouts/Sidebar/Layout';
 import { createClient } from '@/lib/supabaseServer';
 import { redirect } from 'next/navigation';
 import { Button } from '@/components/ui/button';
-import { Label } from '@/components/ui/label';
+// import { Label } from '@/components/ui/label';
 import BarChartComponet from '@/app/dashboard/component';
 import {
 	Card,
@@ -27,85 +27,87 @@ export default async function Dashboard() {
 		<SidebarLayout>
 			{/* Grid Layout */}
 			<div className='grid grid-cols-3 gap-x-4 gap-y-6'>
-				{/* Row-1 */}
-				<div className='flex justify-center items-center'>
-					<Label htmlFor='email'>How can I help you today?</Label>
-				</div>
-				<div className='col-span-2 w-full'>
-					<div className='flex flex-row gap-2'>
-						<Input
-							placeholder='Message to HallEase bot'
-							className='w-full'
-						/>
-						<Button>Go</Button>
+				{/* Column-1|2 */}
+				<div className='col-span-2 grid grid-cols-2 gap-x-4 gap-y-6'>
+					{/* HallEase Bot */}
+					<div className='col-span-2 w-full'>
+						<div className='flex flex-row gap-2'>
+							<Input
+								placeholder='Message to HallEase bot'
+								className='w-full'
+							/>
+							<Button>Go</Button>
+						</div>
+					</div>
+					{/* Recent Bookings */}
+					<div className='flex justify-center w-full items-start'>
+						<Card className='w-full'>
+							<CardHeader>
+								<CardTitle>Recent Bookings</CardTitle>
+								<CardDescription>
+									5 pending approvals
+								</CardDescription>
+							</CardHeader>
+							<CardContent className='grid gap-4'>
+								<div className=' flex items-center space-x-4 rounded-md border p-4'>
+									<div className='flex-1 space-y-1'>
+										<p className='text-sm font-medium leading-none'>
+											Annual General Meeting
+										</p>
+										<p className='text-sm text-muted-foreground'>
+											Tomorrow
+										</p>
+									</div>
+									<Button variant='ghost'>
+										<MoreHorizontal />
+									</Button>
+								</div>
+								<div className=' flex items-center space-x-4 rounded-md border p-4'>
+									<div className='flex-1 space-y-1'>
+										<p className='text-sm font-medium leading-none'>
+											Business Conference
+										</p>
+										<p className='text-sm text-muted-foreground'>
+											Next Friday
+										</p>
+									</div>
+									<Button variant='ghost'>
+										<MoreHorizontal />
+									</Button>
+								</div>
+								<div className=' flex items-center space-x-4 rounded-md border p-4'>
+									<div className='flex-1 space-y-1'>
+										<p className='text-sm font-medium leading-none'>
+											Networking Event
+										</p>
+										<p className='text-sm text-muted-foreground'>
+											February 20
+										</p>
+									</div>
+									<Button variant='ghost'>
+										<MoreHorizontal />
+									</Button>
+								</div>
+							</CardContent>
+							<CardFooter>
+								<Button variant='outline'>
+									<span>See All</span>
+									<ChevronRight />
+								</Button>
+							</CardFooter>
+						</Card>
+					</div>
+
+					{/* Chart */}
+					<div className=''>
+						<h1>Annual Reservation Report</h1>
+						<BarChartComponet />
 					</div>
 				</div>
-				{/* Row-2 */}
-				<div className='flex justify-center w-full items-start'>
-					<Card className='w-full'>
-						<CardHeader>
-							<CardTitle>Recent Bookings</CardTitle>
-							<CardDescription>
-								5 pending approvals
-							</CardDescription>
-						</CardHeader>
-						<CardContent className='grid gap-4'>
-							<div className=' flex items-center space-x-4 rounded-md border p-4'>
-								<div className='flex-1 space-y-1'>
-									<p className='text-sm font-medium leading-none'>
-										Annual General Meeting
-									</p>
-									<p className='text-sm text-muted-foreground'>
-										Tomorrow
-									</p>
-								</div>
-								<Button variant='ghost'>
-									<MoreHorizontal />
-								</Button>
-							</div>
-							<div className=' flex items-center space-x-4 rounded-md border p-4'>
-								<div className='flex-1 space-y-1'>
-									<p className='text-sm font-medium leading-none'>
-										Business Conference
-									</p>
-									<p className='text-sm text-muted-foreground'>
-										Next Friday
-									</p>
-								</div>
-								<Button variant='ghost'>
-									<MoreHorizontal />
-								</Button>
-							</div>
-							<div className=' flex items-center space-x-4 rounded-md border p-4'>
-								<div className='flex-1 space-y-1'>
-									<p className='text-sm font-medium leading-none'>
-										Networking Event
-									</p>
-									<p className='text-sm text-muted-foreground'>
-										February 20
-									</p>
-								</div>
-								<Button variant='ghost'>
-									<MoreHorizontal />
-								</Button>
-							</div>
-						</CardContent>
-						<CardFooter>
-							<Button variant='outline'>
-								<span>See All</span>
-								<ChevronRight />
-							</Button>
-						</CardFooter>
-					</Card>
-				</div>
 
-				<div className=''>
-					<h1>Annual Reservation Report</h1>
-					<BarChartComponet />
-				</div>
+				{/* Column-3 */}
+				<div className=''></div>
 			</div>
-
-			{/* Row-3 */}
 		</SidebarLayout>
 	);
 }
