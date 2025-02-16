a0ae3580b04e4315ecdff6a6ff97f616d0d64039
UdasriHasindu <usrimart@gmail.com>
2025-02-16 15:34:09 +0530

    set user component as isolated login page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,15 @@
-import LoginPage from './front/users/LoginPage';
 import ContactUs from './front/contacts/ContactUs';
 import HomePage from './front/home/HomePage';
 import Reservations from './front/publicReservations/Reservations';
-
+import LoginPage from './login/page';
 
 export default function Home() {
 	return (
 		<div style={{ position: 'relative', height: '100vh' }}>
 			<HomePage />
 			<LoginPage />
-		    <Reservations/>
+			<Reservations />
 			<ContactUs />
-			
 		</div>
 	);
 }
