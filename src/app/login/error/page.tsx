72b7368eaafc5bb63f2b05f385abb87db82170ab
UdasriHasindu <usrimart@gmail.com>
2025-02-16 20:13:44 +0530

    resture the login/error page

--- src/app/login/error/page.tsx
+++ src/app/login/error/page.tsx
@@ -1,10 +1,25 @@
-"use client";
+'use client';
+
+import { useSearchParams } from 'next/navigation';
+import Link from 'next/link';
+import { Button } from '@/components/ui/button';
 
 export default function ErrorPage() {
-  return (
-    <>
-      <h1>Provide correct credentials</h1>
-      <p>Something went wrong, while login</p>
-    </>
-  );
+	const searchParams = useSearchParams();
+	const errorMessage =
+		searchParams.get('message') || 'Something went wrong while logging in.';
+	return (
+		<>
+			<div className='flex flex-col items-center justify-center h-screen text-center gap-2'>
+				<h2 className='text-4xl font-bold'>HallEase</h2>
+				<h1 className='text-2xl font-bold'>Login Error</h1>
+				<p className='text-gray-500'>
+					{decodeURIComponent(errorMessage)}
+				</p>
+				<Button asChild>
+					<Link href='/login'>Try again</Link>
+				</Button>
+			</div>
+		</>
+	);
 }
