54a717f215f8468b2533f6c70031b55e08f0df99
UdasriHasindu <usrimart@gmail.com>
2025-02-19 14:02:35 +0530

    feat(route): add new route for user registration

---
+++ src/app/access-control/register/page.tsx
@@ -0,0 +1,14 @@
+import SidebarLayout from '@/layouts/Sidebar/Layout';
+import React from 'react';
+
+const Registration = () => {
+	return (
+		<>
+			<SidebarLayout>
+				<div>Register a new user</div>
+			</SidebarLayout>
+		</>
+	);
+};
+
+export default Registration;
