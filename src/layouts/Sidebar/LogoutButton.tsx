b24b51e65958dced74412c185a85bb2a103b8049
pathummadhusanka <pathum23200@gmail.com>
2025-02-27 12:00:56 +0530

    chore: style logout button

--- src/layouts/Sidebar/LogoutButton.tsx
+++ src/layouts/Sidebar/LogoutButton.tsx
@@ -26,13 +26,14 @@ export default function LogoutButton() {
 	return (
 		<>
 			{/* Logout Button (Opens the Dialog) */}
-			<button
+			<Button
 				onClick={() => setOpen(true)} // Open dialog on click
-				className='flex flex-row justify-left items-center'
+				className='flex flex-row pl-0 w-full justify-start'
+				variant='ghost'
 			>
 				<LogOut size={20} className='mr-1 text-red-500' />
 				<span className='text-red-500'>Logout</span>
-			</button>
+			</Button>
 
 			{/* Logout Confirmation Modal */}
 			<Dialog open={open} onOpenChange={setOpen}>
