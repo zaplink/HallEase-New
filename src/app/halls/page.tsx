72e5d450b24c709a43416d799ead8dc8b37a192a
pathummadhusanka <pathum23200@gmail.com>
2025-03-13 19:08:15 +0530

    test(): role based component visibility

--- src/app/halls/page.tsx
+++ src/app/halls/page.tsx
@@ -8,9 +8,16 @@ import { columns } from './columns';
 import { DataTable } from './data-table';
 import { getHalls } from './getHalls';
 import { useEffect, useState } from 'react';
+import ProtectedComponent from './ProtectedComponent';
+import { fetchProfile } from '@/lib/fetchProfile';
 
 export default function Hall() {
 	const [halls, setHalls] = useState<HallType[] | null>(null);
+	const [profile, setProfile] = useState<{
+		full_name: string;
+		id: string;
+	} | null>(null);
+	// const [loading, setLoading] = useState(true);
 
 	useEffect(() => {
 		async function fetchHalls() {
@@ -18,7 +25,14 @@ export default function Hall() {
 			setHalls(data);
 		}
 
+		async function fetchUser() {
+			const profileData = await fetchProfile();
+			setProfile(profileData);
+			// setLoading(false);
+		}
+
 		fetchHalls();
+		fetchUser();
 	}, []);
 
 	return (
@@ -29,7 +43,14 @@ export default function Hall() {
 					{halls === null ? (
 						<p>Loading halls...</p>
 					) : (
-						<DataTable columns={columns} data={halls} />
+						<>
+							{profile === null ? (
+								<p>Loading profile...</p>
+							) : (
+								<ProtectedComponent profileId={profile?.id} />
+							)}
+							<DataTable columns={columns} data={halls} />
+						</>
 					)}
 				</div>
 			</SidebarLayout>
