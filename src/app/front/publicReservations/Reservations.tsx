e957b39f053b7fe2ae65549d245d3fd0ac83747e
UdasriHasindu <usrimart@gmail.com>
2025-03-15 15:47:55 +0530

    refactor(front/public): seperate card from tabs, implement individually

--- src/app/front/publicReservations/Reservations.tsx
+++ src/app/front/publicReservations/Reservations.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
-import Image from 'next/image';
-import { Card, CardContent } from '@/components/ui/card';
-import { AuditoriumForm } from './auditoriumForm';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs';
+import Auditorium from './Auditorium';
 
 const Reservations = () => {
 	return (
@@ -24,32 +22,7 @@ const Reservations = () => {
 				</TabsList>
 
 				<TabsContent value='auditorium'>
-					<div className='bg-white shadow-lg rounded-2xl overflow-hidden w-full max-w-7xl lg:h-[80vh] grid grid-cols-1 lg:grid-cols-2'>
-						{/* Left Section - Image */}
-						<div className='relative h-100 lg:h-auto'>
-							<Image
-								src='/auditorium.jpg'
-								alt='Signup'
-								layout='fill'
-								objectFit='cover'
-								className='rounded-l-2xl'
-							/>
-						</div>
-
-						{/* Right Section - Signup Form */}
-						<Card className='p-8 rounded-none overflow-y-auto'>
-							<p className='text-gray-500 text-lg'>
-								Your Event, Our Venue.
-							</p>
-							<h2 className='text-2xl font-bold text-gray-800'>
-								Reserve the Auditorium Today!
-							</h2>
-
-							<CardContent className='space-y-4 mt-5'>
-								<AuditoriumForm />
-							</CardContent>
-						</Card>
-					</div>
+					<Auditorium />
 				</TabsContent>
 				<TabsContent value='studio'>
 					<p>Implement reservations for studio</p>
